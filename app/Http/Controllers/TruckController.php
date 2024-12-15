f20e782ddef779daffc9b11ecc6c1aae74fd1cd8
Rami Hamdan <rami.web@hotmail.com>
2024-12-15 11:55:33 +0300

    fix truck controller

--- app/Http/Controllers/TruckController.php
+++ app/Http/Controllers/TruckController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\SurveyResource;
 use App\Models\Truck;
 use App\Http\Requests\truckStoreRequest;
 use App\Http\Requests\truckUpdateRequest;
+use App\Http\Resources\TruckResource;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
@@ -44,35 +45,27 @@ class TruckController extends Controller
         $data = $request->validated();
 
         // Check if image was given and save on local file system
-        if (isset($data['image'])) {
-            $relativePath = $this->saveImage($data['image']);
-            $data['image'] = $relativePath;
-        }
 
-        $survey = Survey::create($data);
 
-        // Create new questions
-        foreach ($data['questions'] as $question) {
-            $question['survey_id'] = $survey->id;
-            $this->createQuestion($question);
-        }
+        $survey = Truck::create($data);
 
-        return new SurveyResource($survey);
+
+        return new TruckResource($survey);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Survey  $survey
+     * @param  \App\Models\Truck  $Truck
      * @return \Illuminate\Http\Response
      */
-    public function show(Survey $survey, Request $request)
+    public function show(Truck $truck, Request $request)
     {
         $user = $request->user();
-        if ($user->id !== $survey->user_id) {
+        if ($user->id !== $truck->user_id) {
             return abort(403, 'Unauthorized action');
         }
-        return new SurveyResource($survey);
+        return new TruckResource($truck);
     }
 
     /**
@@ -82,215 +75,41 @@ class TruckController extends Controller
      * @param  \App\Models\Survey  $survey
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateSurveyRequest $request, Survey $survey)
+    public function update(truckUpdateRequest $request, Truck $truck)
     {
         $data = $request->validated();
 
-        // Check if image was given and save on local file system
-        if (isset($data['image'])) {
-            $relativePath = $this->saveImage($data['image']);
-            $data['image'] = $relativePath;
+        // Update truck in the database
+        $truck->update($data);
 
-            // If there is an old image, delete it
-            if ($survey->image) {
-                $absolutePath = public_path($survey->image);
-                File::delete($absolutePath);
-            }
-        }
-
-        // Update survey in the database
-        $survey->update($data);
-
-        // Get ids as plain array of existing questions
-        $existingIds = $survey->questions()->pluck('id')->toArray();
-        // Get ids as plain array of new questions
-        $newIds = Arr::pluck($data['questions'], 'id');
-        // Find questions to delete
-        $toDelete = array_diff($existingIds, $newIds);
-        //Find questions to add
-        $toAdd = array_diff($newIds, $existingIds);
-
-        // Delete questions by $toDelete array
-        SurveyQuestion::destroy($toDelete);
-
-        // Create new questions
-        foreach ($data['questions'] as $question) {
-            if (in_array($question['id'], $toAdd)) {
-                $question['survey_id'] = $survey->id;
-                $this->createQuestion($question);
-            }
-        }
-
-        // Update existing questions
-        $questionMap = collect($data['questions'])->keyBy('id');
-        foreach ($survey->questions as $question) {
-            if (isset($questionMap[$question->id])) {
-                $this->updateQuestion($question, $questionMap[$question->id]);
-            }
-        }
-
-        return new SurveyResource($survey);
+        return new TruckResource($truck);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Survey  $survey
+     * @param  \App\Models\Truck  $truck
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Survey $survey, Request $request)
+    public function destroy(Truck $truck, Request $request)
     {
         $user = $request->user();
-        if ($user->id !== $survey->user_id) {
+        if ($user->id !== $truck->user_id) {
             return abort(403, 'Unauthorized action.');
         }
 
-        $survey->delete();
-
-        // If there is an old image, delete it
-        if ($survey->image) {
-            $absolutePath = public_path($survey->image);
-            File::delete($absolutePath);
-        }
+        $truck->delete();
 
         return response('', 204);
     }
 
-
-    /**
-     * Save image in local file system and return saved image path
-     *
-     * @param $image
-     * @throws \Exception
-     * @author Zura Sekhniashvili <zurasekhniashvili@gmail.com>
-     */
-    private function saveImage($image)
+    public function getById(Truck $truck)
     {
-        // Check if image is valid base64 string
-        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)) {
-            // Take out the base64 encoded text without mime type
-            $image = substr($image, strpos($image, ',') + 1);
-            // Get file extension
-            $type = strtolower($type[1]); // jpg, png, gif
-
-            // Check if file is an image
-            if (!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])) {
-                throw new \Exception('invalid image type');
-            }
-            $image = str_replace(' ', '+', $image);
-            $image = base64_decode($image);
-
-            if ($image === false) {
-                throw new \Exception('base64_decode failed');
-            }
-        } else {
-            throw new \Exception('did not match data URI with image data');
-        }
-
-        $dir = 'images/';
-        $file = Str::random() . '.' . $type;
-        $absolutePath = public_path($dir);
-        $relativePath = $dir . $file;
-        if (!File::exists($absolutePath)) {
-            File::makeDirectory($absolutePath, 0755, true);
-        }
-        file_put_contents($relativePath, $image);
-
-        return $relativePath;
-    }
-
-    /**
-     * Create a question and return
-     *
-     * @param $data
-     * @return mixed
-     * @throws \Illuminate\Validation\ValidationException
-     * @author Zura Sekhniashvili <zurasekhniashvili@gmail.com>
-     */
-    private function createQuestion($data)
-    {
-        if (is_array($data['data'])) {
-            $data['data'] = json_encode($data['data']);
-        }
-        $validator = Validator::make($data, [
-            'question' => 'required|string',
-            'type' => [
-                'required',
-                new Enum(QuestionTypeEnum::class)
-            ],
-            'description' => 'nullable|string',
-            'data' => 'present',
-            'survey_id' => 'exists:App\Models\Survey,id'
-        ]);
-
-        return SurveyQuestion::create($validator->validated());
-    }
-
-    /**
-     * Update a question and return true or false
-     *
-     * @param \App\Models\SurveyQuestion $question
-     * @param                            $data
-     * @return bool
-     * @throws \Illuminate\Validation\ValidationException
-     * @author Zura Sekhniashvili <zurasekhniashvili@gmail.com>
-     */
-    private function updateQuestion(SurveyQuestion $question, $data)
-    {
-        if (is_array($data['data'])) {
-            $data['data'] = json_encode($data['data']);
-        }
-        $validator = Validator::make($data, [
-            'id' => 'exists:App\Models\SurveyQuestion,id',
-            'question' => 'required|string',
-            'type' => ['required', new Enum(QuestionTypeEnum::class)],
-            'description' => 'nullable|string',
-            'data' => 'present',
-        ]);
-
-        return $question->update($validator->validated());
-    }
-
-    public function getBySlug(Survey $survey)
-    {
-        if (!$survey->status) {
+        if (!$truck->status) {
             return response("", 404);
         }
 
-        $currentDate = new \DateTime();
-        $expireDate = new \DateTime($survey->expire_date);
-        if ($currentDate > $expireDate) {
-            return response("", 404);
-        }
 
-        return new SurveyResource($survey);
-    }
-
-    public function storeAnswer(StoreSurveyAnswerRequest $request, Survey $survey)
-    {
-        $validated = $request->validated();
-
-        $surveyAnswer = SurveyAnswer::create([
-            'survey_id' => $survey->id,
-            'start_date' => date('Y-m-d H:i:s'),
-            'end_date' => date('Y-m-d H:i:s'),
-        ]);
-
-        foreach ($validated['answers'] as $questionId => $answer) {
-            $question = SurveyQuestion::where(['id' => $questionId, 'survey_id' => $survey->id])->get();
-            if (!$question) {
-                return response("Invalid question ID: \"$questionId\"", 400);
-            }
-
-            $data = [
-                'survey_question_id' => $questionId,
-                'survey_answer_id' => $surveyAnswer->id,
-                'answer' => is_array($answer) ? json_encode($answer) : $answer
-            ];
-
-            $questionAnswer = SurveyQuestionAnswer::create($data);
-        }
-
-        return response("", 201);
+        return new TruckResource($truck);
     }
 }
